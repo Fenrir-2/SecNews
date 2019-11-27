f3dbdd0969d22bd1e020dec6c617975b0e5eddb0
P1nk-P0nk <kieranlependeven@gmail.com>
2019-11-27 01:42:01 +0100

    Creating category_push.sql to add category to db

---
+++ category_push.sql
@@ -0,0 +1,9 @@
+INSERT INTO CATEGORIES (Nom,wordlist) VALUES ('hardware','electronique,electronics,hardware,materiels');
+INSERT INTO CATEGORIES (Nom,wordlist) VALUES ('osint','osint,geoint,sigint,humint,masint,socmint,rumint,dorks,dorking,shodan,dork,opsec');
+INSERT INTO CATEGORIES (Nom,wordlist) VALUES ('threats','menaces virus,malware');
+INSERT INTO CATEGORIES (Nom,wordlist) VALUES ('apple','apple,osx,ios,iphone,iwatch,itv,apple watch,apple tv');
+INSERT INTO CATEGORIES (Nom,wordlist) VALUES ('android','android');
+INSERT INTO CATEGORIES (Nom,wordlist) VALUES ('windows','windows,xp,vista,sam,ad,active directory,w7');
+INSERT INTO CATEGORIES (Nom,wordlist) VALUES ('winux','luks,ubuntu,fedora,kde,xfce,gnome,archlinux');
+INSERT INTO CATEGORIES (Nom,wordlist) VALUES ('malware','trojan,worm,ransomware,keylogger,rootkit,bootkit,banker');
+INSERT INTO CATEGORIES (Nom,wordlist) VALUES ('web','apache,django,cms,php,js,javascript,v8');
