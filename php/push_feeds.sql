f5b618bb27b5afaff93f09933467fb0d110dcebf
P1nk-P0nk <kieranlependeven@gmail.com>
2019-11-27 03:22:02 +0100

    Change typo error in push_feeds.php

--- php/push_feeds.sql
+++ php/push_feeds.sql
@@ -1,8 +1,8 @@
-INSERT INTO SITES (Url,nom) VALUES ('TheHackerNews','https://feeds.feedburner.com/TheHackersNews?format=xml,TheHackersNews'); 
-INSERT INTO SITES (Url,nom) VALUES ('YesWeHack','https://blog.yeswehack.com/feed/,Yes We Hack'); 
-INSERT INTO SITES (Url,nom) VALUES ('CERT-FR','https://www.cert.ssi.gouv.fr/alerte/feed/,CERT-FR'); 
-INSERT INTO SITES (Url,nom) VALUES ('Fumik0','https://fumik0.com/feed/,Fumik0'); 
-INSERT INTO SITES (Url,nom) VALUES ('Security Soup','https://security-soup.net/feed/,Security Soup'); 
-INSERT INTO SITES (Url,nom) VALUES ('No Limit Secu','https://www.nolimitsecu.fr/feed/podcast/,No Limit Secu'); 
-INSERT INTO SITES (Url,nom) VALUES ('Zataz','https://www.zataz.com/rss/zataz-news.rss,Zataz'); 
-INSERT INTO SITES (Url,nom) VALUES ('Cyberguerre','https://cyberguerre.numerama.com/feed/,Cyberguerre (Numerama)'); 
+INSERT INTO SITES (nom,Url) VALUES ('TheHackerNews','https://feeds.feedburner.com/TheHackersNews?format=xml,TheHackersNews'); 
+INSERT INTO SITES (nom,Url) VALUES ('YesWeHack','https://blog.yeswehack.com/feed/,Yes We Hack'); 
+INSERT INTO SITES (nom,Url) VALUES ('CERT-FR','https://www.cert.ssi.gouv.fr/alerte/feed/,CERT-FR'); 
+INSERT INTO SITES (nom,Url) VALUES ('Fumik0','https://fumik0.com/feed/,Fumik0'); 
+INSERT INTO SITES (nom,Url) VALUES ('Security Soup','https://security-soup.net/feed/,Security Soup'); 
+INSERT INTO SITES (nom,Url) VALUES ('No Limit Secu','https://www.nolimitsecu.fr/feed/podcast/,No Limit Secu'); 
+INSERT INTO SITES (nom,Url) VALUES ('Zataz','https://www.zataz.com/rss/zataz-news.rss,Zataz'); 
+INSERT INTO SITES (nom,Url) VALUES ('Cyberguerre','https://cyberguerre.numerama.com/feed/,Cyberguerre (Numerama)'); 
